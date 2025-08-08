2e04d09ab281a84ec8c24cedf54e1971ff575932
254francis <86851202+254francis@users.noreply.github.com>
2025-08-08 13:18:26 +0300

    Update bob.py
    Making the connection 2-way

--- CreditVault/bob.py
+++ CreditVault/bob.py
@@ -1,21 +1,46 @@
 import socket
-from Crypto.Util.number import bytes_to_long
+from Crypto.Util.number import getPrime, inverse, bytes_to_long, long_to_bytes
+
+def generate_keys():
+    p = getPrime(512)
+    q = getPrime(512)
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = 65537
+    d = inverse(e, phi)
+    return (n, e), (n, d)
+
+# Generate Bob's keys
+bob_public, bob_private = generate_keys()
 
 # Connect to Alice
 client = socket.socket()
 client.connect(('localhost', 12345))
 
-# Receive public key
-data = client.recv(4096).decode()
-n, e = map(int, data.split(','))
+# Receive Alice's public key
+alice_key_data = client.recv(4096).decode()
+alice_n, alice_e = map(int, alice_key_data.split(','))
+alice_public = (alice_n, alice_e)
 
-# Input message
-message = input("[Bob] Enter a message to send to Alice: ")
-m = bytes_to_long(message.encode())
+# Send Bob's public key to Alice
+client.send(f"{bob_public[0]},{bob_public[1]}".encode())
+print("[Bob] Public key exchange complete.")
 
-# Encrypt and send
-cipher = pow(m, e, n)
-client.send(str(cipher).encode())
-print("[Bob] Ciphertext sent!")
+while True:
+    # Send a message to Alice
+    message = input("[Bob] Enter message: ")
+    if message.lower() == "exit":
+        client.send("exit".encode())
+        break
+    cipher_out = pow(bytes_to_long(message.encode()), alice_public[1], alice_public[0])
+    client.send(str(cipher_out).encode())
+
+    # Receive Alice's reply
+    cipher_in = client.recv(4096).decode()
+    if cipher_in == "exit":
+        print("[Bob] Alice ended the chat.")
+        break
+    decrypted = pow(int(cipher_in), bob_private[1], bob_private[0])
+    print(f"[Alice]: {long_to_bytes(decrypted).decode()}")
 
 client.close()
